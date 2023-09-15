dcdf6b51dcd466697d1f7105f7399a2d121715b2
Sascha <Tohwa.gaming@gmail.com>
2023-09-15 14:52:54 +0200

    added game loop

---
+++ State_Machine/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_Machine
+{
+    internal class GameManager
+    {
+        public bool item1 = false;
+        public bool item2 = false;
+        public bool item3 = false;
+    }
+}
--- State_Machine/StateMachine.cs
+++ State_Machine/StateMachine.cs
@@ -12,7 +12,7 @@ namespace State_Machine
 
         public void Start()
         {
-            curState = new RoomBaseState();
+            //curState = 
             curState.EnterState();
         }
 
